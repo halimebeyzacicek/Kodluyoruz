2c6b546fb7e298756613184c39a7c7fec00d6666
halimebeyzacicek <hbcicek25@gmail.com>
2021-10-01 01:54:07 +0300

    add_capsulation_example

---
+++ Object Oriented Programming/Encapsulation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace kodluyoruz_encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kitap book = new Kitap("Beyaz Geceler", -202, "Dostoyevski");
+            //book.yazdir(book.sayfaSayisi); //kitap class ı içerisinde değişkenleri private olduğu için onlara bu şekilde ulaşamayız.
+           
+            int sayfaSayisi =book.getSayfaSayisi();
+            book.yazdir(sayfaSayisi);
+
+            book.setSayfaSayisi(-10);//set ile sayfa sayisini değiştirdik. 
+            int sayfaSayisi1 = book.getSayfaSayisi();
+            book.yazdir(sayfaSayisi1);
+
+            Console.ReadLine();
+        }
+    }
+}
